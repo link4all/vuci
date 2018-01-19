b89c99c7697bdd3bb61cb4c5865aae48f2a950e3
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-01-19 22:19:33 +0800

    Fix syntax err
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/App.vue
+++ vuci-ui-base/src/src/App.vue
@@ -5,15 +5,16 @@
 </template>
 
 <style>
-	html,body{
+	html, body{
 		width: 100%;
 	    height: 100%;
 	    background: #f0f0f0;
 	    overflow: hidden;
-	}
+    }
+
 	#app {
 	    width: 100%;
 	    height: 100%;
 	}
-</style>>
+</style>
 
