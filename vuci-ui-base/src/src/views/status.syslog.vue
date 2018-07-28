41dc3a20ea9f3d6a7a4cf16ed53e0acba02af083
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-07-28 15:12:17 +0800

    Optimize view
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/views/status.syslog.vue
+++ vuci-ui-base/src/src/views/status.syslog.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Input v-model="log" type="textarea" :autosize="{minRows: 10, maxRows: 25}" readonly></Input>
+        <Input v-model="log" type="textarea" :autosize="true" readonly></Input>
     </div>
 </template>
 
